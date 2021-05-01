e1d54adb279be82785dde2d253690d2aa7cde9b0
Rahul <rahulkannan144@gmail.com>
2021-05-01 13:51:01 +0530

    Added swagger

--- server/server.js
+++ server/server.js
@@ -1,23 +1,27 @@
-const connection = require("./db/model");
-require("dotenv").config();
-const institution = require('./institution/institution')
-const user = require('../server/user/auth')
-
-
 const express = require('express')
 const router = express.Router();
 const app = express()
 const port = 3000
 
+const connection = require("./db/model");
+require("dotenv").config();
+const institution = require('./institution/institution')
+const user = require('../server/user/auth')
+const swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('./swagger.json');
+
+
+app.use(
+    '/api-docs',
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerDocument)
+  );
+
 app.use(express.json());
 
 app.get('/user/:id',  user.getUser)
-
 app.post('/user', user.addUser)
-
-
 app.get('/institution/:id', institution.getInstitution)
-
 app.post('/institution', institution.addInstitution)
 
 app.listen(port);
